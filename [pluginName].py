223191be8f72ab619c191051a7cd6f5a16b08040
Parth Bansal <33196466+parthbansal05@users.noreply.github.com>
2023-01-19 10:56:48 +0530

    added basis code for the plugin RouteManager, [pluginName].py, configuration.conf, _js folder

--- [pluginName].py
+++ [pluginName].py
@@ -1,1 +1,20 @@
 import server_pkg.essentials as e
+from Plugins.PluginFiles.plugin import RouteManager
+
+RouteManager.initRoute(self)
+
+
+while(True):
+    while(self.new == False):
+        time.sleep(self.sleepTime)
+    data = self.data
+    self.new = False
+
+    if data.target == "*":
+        # a broadcast msg
+        # follow Category format
+        pass
+    else:
+        # a direct msg
+        # follow HOOK format
+        pass
---
+++ [pluginName]_pkt/RouteManager.py
@@ -0,0 +1,13 @@
+import server_pkg.essentials as e
+
+def initRoute(self):
+    # layout
+    @self.app.route("/pluginHome", methods=["GET"], strict_slashes=False)
+    @e.fl.login_required
+    def pluginHome():
+        return e.render_template('/plugin_edpt/pluginHome.html')
+
+    @self.app.route("/pluginAdmin", methods=["GET"], strict_slashes=False)
+    @e.admin_required
+    def pluginAdminDashboard():
+        return e.render_template("/plugin_edpt/pluginAdmin.html")
